c1d2dd954c3cfc4b4a2c7f05026e2189927b1aee
Mark <134824619+Mark-shija@users.noreply.github.com>
2023-11-15 01:44:32 +0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -13,7 +13,7 @@
  * Return: Number of chars printed
  */
 int print_char(va_list types, char buffer[],
-	int flags, width, precision, size)
+	int flags, int width, int precision, int size)
 {
 	char c = va_arg(types, int);
 
@@ -31,7 +31,7 @@ int print_char(va_list types, char buffer[],
  * Return: Number of chars printed
  */
 int print_string(va_list types, char buffer[],
-	int flags, width, precision, size)
+	int flags, int width, int precision, int size)
 {
 	int length = 0, i;
 	char *str = va_arg(types, char *);
