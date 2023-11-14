7e4b98b95be0d7f0ee51896590f2b55bf1cf68b6
Mark <134824619+Mark-shija@users.noreply.github.com>
2023-11-15 00:52:51 +0300

    Update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -8,7 +8,8 @@
  */
 int get_flags(const char *format, int *i)
 {
-	int j, curr_i;
+	int j;
+	int curr_i;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
